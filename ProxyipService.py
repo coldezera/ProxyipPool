3891adcaa2535b9cf5415d5265734079b2326265
coldezera <616775154@qq.com>
2017-11-04 19:52:25 +0800

    code

---
+++ ProxyipService.py
@@ -0,0 +1,64 @@
+# !/usr/bin/python
+# -*- coding: utf8 -*-
+
+import win32service
+import win32serviceutil
+import win32event
+import os, time
+from aps import ProxyIPAPS
+import traceback
+
+"""
+爬取代理ip网页ip10分钟爬取一次
+UnverfiedIP中的ip没有之后强制crawl重新爬取一次，重置crawl倒计时
+verfiedIP中的ip数量少于10之后强制验证UnverfiedIP一次，重置验证UnverfiedIP倒计时
+验证UnverfiedIP45秒一次
+验证verfiedIP每15秒一次
+"""
+
+
+class ProxyIPPool(win32serviceutil.ServiceFramework):
+    _svc_name_ = 'ProxyIPPoolService'
+    _svc_display_name_ = 'ProxyIPPoolService'
+    _svc_description_ = 'a task is crawl the proxy ip in the Internet and check the ip is useable'
+
+    def __init__(self, args):
+        win32serviceutil.ServiceFramework.__init__(self, args)
+        self.hWaitStop = win32event.CreateEvent(None, 0, 0, None)
+        self.isAlive = True
+
+    def SvcDoRun(self):
+        self.ReportServiceStatus(win32service.SERVICE_RUNNING)
+        try:
+            sched = ProxyIPAPS()
+            sched.run()
+        except Exception as e:
+            print(repr(e))
+            print(traceback.print_exc())
+        while self.isAlive:
+            time.sleep(2)
+        win32event.WaitForSingleObject(self.hWaitStop, win32event.INFINITE)
+
+    def SvcStop(self):
+        self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
+        win32event.SetEvent(self.hWaitStop)
+        self.isAlive = False
+
+
+if __name__ == '__main__':
+    import sys
+    import servicemanager
+
+    if len(sys.argv) == 1:
+        try:
+            evtsrc_dll = os.path.abspath(servicemanager.__file__)
+            servicemanager.PrepareToHostSingle(ProxyIPPool)
+            servicemanager.Initialize('ProxyIPPool', evtsrc_dll)
+            servicemanager.StartServiceCtrlDispatcher()
+        except win32service.error as details:
+            import winerror
+
+            if details == winerror.ERROR_FAILED_SERVICE_CONTROLLER_CONNECT:
+                win32serviceutil.usage()
+    else:
+        win32serviceutil.HandleCommandLine(ProxyIPPool)
---
+++ apschedulerjob.py
@@ -0,0 +1,95 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+from mongo.Mongopy import ConnectMongoProTable
+import configparser
+import CtrlFunc
+from threading import Lock
+import time
+
+
+def GetDB():
+    """
+    读取配置文件链接数据库
+    :return: 数据库对象
+    """
+    try:
+        mongocfg = configparser.ConfigParser()
+        file = mongocfg.read('E:/spider-progress/getproxyip/sched_conf.ini')
+    except:
+        Exception('configuration mongodb failed! plz check out sched_conf.ini!')
+    if len(file) == 0:
+        Exception('config file is empty!')
+    sections = mongocfg.sections()
+    return ConnectMongoProTable(mongocfg.get(sections[0], 'host'),
+                                int(mongocfg.get(sections[0], 'port')),
+                                user={
+                                    'username': mongocfg.get(sections[0], 'user_name'),
+                                    'password': mongocfg.get(sections[0], 'password')
+                                })
+
+
+class ProxyIPAPS:
+    main_process_sche = None
+    checker_sche = None
+
+    def __init__(self):
+        self.database = GetDB()
+        job_defaults = {
+            'coalesce': True,
+            'max_instances': 1
+        }
+        self.main_process_sche = BackgroundScheduler(job_defaults=job_defaults)
+        self.checker_sche = BackgroundScheduler(job_defaults=job_defaults)
+        self.lock = Lock()
+
+    def __main_process(self):
+
+        @self.main_process_sche.scheduled_job('cron', minute='*/10')    # 爬取代理ip网页JOB
+        def crawl_job():
+            self.lock.acquire()
+            CtrlFunc.CrawlToUvipDB(self.database.UnverifiedIP)
+            self.lock.release()
+
+        @self.main_process_sche.scheduled_job('cron', second='*/60')    # 验证UnverfiedIP JOB
+        def checkuvip_job():
+            self.lock.acquire()
+            CtrlFunc.CheckUvipToVip(self.database.UnverifiedIP, self.database.verifiedIP)
+            self.lock.release()
+
+        @self.main_process_sche.scheduled_job('cron', second='*/20')    # 验证verfiedIP JOB
+        def checkvip_job():
+            self.lock.acquire()
+            CtrlFunc.CheckVipToUvip(self.database.UnverifiedIP, self.database.verifiedIP)
+            self.lock.release()
+
+        pass
+
+    def __check_process(self):
+        @self.checker_sche.scheduled_job('cron', second='*/30') # 强制crawl JOB
+        def force_crwal():
+            self.lock.acquire()
+            if CtrlFunc.CollectionCount(self.database.UnverifiedIP) < 0:
+                CtrlFunc.CrawlToUvipDB(self.database.UnverifiedIP)
+            self.lock.release()
+
+        @self.checker_sche.scheduled_job('cron', second='*/10') # 强制验证UnverfiedIP JOB
+        def force_checkuvip():
+            self.lock.acquire()
+            if CtrlFunc.CollectionCount(self.database.verifiedIP) < 10:
+                CtrlFunc.CheckUvipToVip(self.database.UnverifiedIP, self.database.verifiedIP)
+            self.lock.release()
+
+    # def __test_process(self):
+    #     @self.main_process_sche.scheduled_job('cron', second='*/2')
+    #     def getdata_job():
+    #         print(CtrlFunc.Get_OneIP(self.database.verifiedIP))
+
+    def run(self):
+        self.__main_process()
+        self.__check_process()
+        self.main_process_sche.start()
+        self.check_process().start()
+if __name__ == '__main__':
+    a = ProxyIPAPS()
+    a.run()
+    while True:
+        time.sleep(2)
